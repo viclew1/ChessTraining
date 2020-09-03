45bfcc0d5d9435125cffbe9873b34e02d327356b
Lewon <Victor.Lewon@amersports.com>
2020-09-03 15:30:35 +0200

    Les boutons start et reset font maintenant ce qu'ils doivent faire

--- src/main/java/fr/lewon/gui/GameFrameController.java
+++ src/main/java/fr/lewon/gui/GameFrameController.java
@@ -5,33 +5,79 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 public class GameFrameController implements Initializable {
 
 	@FXML
 	private AnchorPane chessGamePane;
 
+	private StackPane contentPane;
+
+	private Rectangle disabledRectangle;
+
 	@FXML
 	private TextArea logsArea;
+	@FXML
+	private Button startBtn;
+	@FXML
+	private Button resetBtn;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		contentPane = new StackPane();
+		disabledRectangle = new Rectangle();
+		disabledRectangle.setFill(Color.BLACK);
+		disabledRectangle.setOpacity(0.5);
+		disabledRectangle.widthProperty().bind(contentPane.widthProperty());
+		disabledRectangle.heightProperty().bind(contentPane.heightProperty());
+		initChessGamePane();
+		disable();
+	}
+
+	private void initChessGamePane() {
 		ChessPane chessPane = new ChessPane();
-		chessGamePane.getChildren().add(chessPane);
-		AnchorPane.setLeftAnchor(chessPane, 0d);
-		AnchorPane.setRightAnchor(chessPane, 0d);
-		AnchorPane.setTopAnchor(chessPane, 0d);
-		AnchorPane.setBottomAnchor(chessPane, 0d);
+		AnchorPane.setLeftAnchor(contentPane, 0d);
+		AnchorPane.setRightAnchor(contentPane, 0d);
+		AnchorPane.setTopAnchor(contentPane, 0d);
+		AnchorPane.setBottomAnchor(contentPane, 0d);
+		
+		contentPane.getChildren().add(chessPane);
+		chessGamePane.getChildren().add(contentPane);
+
+	}
+
+	private void disable() {
+		chessGamePane.setDisable(true);
+		if (!contentPane.getChildren().contains(disabledRectangle)) {
+			contentPane.getChildren().add(disabledRectangle);
+		}
+		startBtn.setDisable(false);
+		resetBtn.setDisable(true);
+	}
+
+	private void enable() {
+		chessGamePane.setDisable(false);
+		if (contentPane.getChildren().contains(disabledRectangle)) {
+			contentPane.getChildren().remove(disabledRectangle);
+		}
+		startBtn.setDisable(true);
+		resetBtn.setDisable(false);
 	}
 
 	public void startGame() {
-		System.out.println("start");
+		enable();
 	}
 
 	public void resetGame() {
-		System.out.println("reset");
+		chessGamePane.getChildren().clear();
+		initChessGamePane();
+		disable();
 	}
 
 }
